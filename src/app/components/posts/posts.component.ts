45e184ff16ad06753d6c486702493686f570f19b
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-07-07 22:05:58 +0200

    Added Posts

---
+++ src/app/components/posts/posts.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+  response: any;
+  constructor(private client: HttpClient) { }
+
+  ngOnInit(): void {
+    this.client
+      .get('http://localhost:3000/post')
+      .subscribe(data => {
+        this.response = data;
+        console.log(data);
+      });
+  }
+
+}
