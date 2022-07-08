4207e45a750c2a3190b6f5912adad490472c096a
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-07-08 21:58:28 +0200

    Added LoginComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { InfoComponent } from './components/info/info.component';
 import { PostsComponent } from './components/posts/posts.component';
-import {RouterModule} from "@angular/router";
-import {HttpClientModule} from "@angular/common/http";
+import { RouterModule } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
 import { HomeComponent } from './components/home/home.component';
-import {routes} from "./app-routing.module";
+import { routes} from "./app-routing.module";
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -16,14 +20,18 @@ import {routes} from "./app-routing.module";
     HeaderComponent,
     InfoComponent,
     PostsComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
-  imports: [
-    RouterModule.forRoot(routes),
-    HttpClientModule,
-    BrowserModule,
-    RouterModule
-  ],
+    imports: [
+      FormsModule,
+      RouterModule.forRoot(routes),
+      HttpClientModule,
+      BrowserModule,
+      RouterModule,
+      ReactiveFormsModule,
+      FlexLayoutModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
