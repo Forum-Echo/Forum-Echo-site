a9ea53b33f598baec426b1a8d6f3c0f586878b63
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-07-09 23:51:23 +0200

    Added theme.scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -11,8 +18,6 @@ import { RouterModule } from "@angular/router";
 import { HttpClientModule } from "@angular/common/http";
 import { HomeComponent } from './components/home/home.component';
 import { routes} from "./app-routing.module";
-import { LoginComponent } from './components/login/login.component';
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -21,7 +26,7 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
     InfoComponent,
     PostsComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
   ],
     imports: [
       FormsModule,
@@ -31,6 +36,13 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
       RouterModule,
       ReactiveFormsModule,
       FlexLayoutModule,
+      MatFormFieldModule,
+      MatInputModule,
+      BrowserAnimationsModule,
+      BrowserModule,
+      MatButtonModule,
+      MatCardModule,
+      MatToolbarModule,
     ],
   providers: [],
   bootstrap: [AppComponent]
