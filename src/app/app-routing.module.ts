bf7ba996e7356d0665d738f2f89dfdc6ea7f88a2
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-07-11 05:28:39 +0200

    Added /newpost route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./components/home/home.component";
 import { LoginComponent } from "./components/login/login.component";
+import {NewPostComponent} from "./components/newpost/newpost.component";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'newpost', component: NewPostComponent },
 ]
