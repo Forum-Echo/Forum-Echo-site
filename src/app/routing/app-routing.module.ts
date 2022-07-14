2bca31b9bec46f4f386baa068f43b5fc8a71a6a5
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-07-14 02:42:56 +0200

    Moved app-routing.module.ts into routing folder

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { RouterModule, Routes } from '@angular/router';
 
 // Components
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { NewPostComponent } from './components/newpost/newpost.component';
-import { UserSettingsComponent } from './components/user-settings/user-settings.component';
+import { HomeComponent } from '../components/home/home.component';
+import { LoginComponent } from '../components/login/login.component';
+import { NewPostComponent } from '../components/newpost/newpost.component';
+import { UserSettingsComponent } from '../components/user-settings/user-settings.component';
 import { NgModule } from '@angular/core';
-import { EditpostComponent } from './components/editpost/editpost.component';
-import { RegisterComponent } from './components/register/register.component';
-import {NotfoundComponent} from "./components/notfound/notfound.component";
+import { EditpostComponent } from '../components/editpost/editpost.component';
+import { RegisterComponent } from '../components/register/register.component';
+import {NotfoundComponent} from "../components/notfound/notfound.component";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
