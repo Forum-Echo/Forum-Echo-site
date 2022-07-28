3d7bcc9806a5d31e79f406a6befde36bd972d9d7
avanloo01 <arthur@vanloo.biz>
2022-07-28 09:11:47 +0200

    New logo, hide add on /newpost, better info tab

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import {AuthService} from "../../http/services/auth.service";
 
 @Component({
@@ -8,12 +9,15 @@ import {AuthService} from "../../http/services/auth.service";
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   isLoggedIn() {
     return this.authService.isLoggedIn();
+  }
+  showAdd(){
+    return (this.router.url != '/newpost');
   }
 }
