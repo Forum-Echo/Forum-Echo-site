458838936f50e3af94c8aabeea5fd88213257e49
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-08-22 22:21:46 +0200

    Added reset route

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -12,7 +12,8 @@ import {NotfoundComponent} from "../components/notfound/notfound.component";
 import { AboutComponent } from '../components/about/about.component';
 import { PrivacyComponent } from '../components/privacy/privacy.component';
 import { TermsComponent } from '../components/terms/terms.component';
-import {VerifyComponent} from "../components/verify/verify.component";
+import { VerifyComponent } from "../components/verify/verify.component";
+import {ResetComponent} from "../components/reset/reset.component";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, data: { animation: 'HomePage' } },
@@ -24,7 +25,8 @@ export const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'privacy', component: PrivacyComponent },
   { path: 'terms', component: TermsComponent },
-  { path: 'verify/:Id' , component: VerifyComponent, data: { animation: 'VerifyPage' } },
+  { path: 'verify/:Id', component: VerifyComponent, data: { animation: 'VerifyPage' } },
+  { path: 'reset/:token', component: ResetComponent, data: { animation: 'ResetPage' } },
   { path: '**', component: NotfoundComponent },
 ];
 
