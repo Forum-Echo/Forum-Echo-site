b16b565ac5ac26352b5ab39cbd9e450ada22654b
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-08-26 01:50:01 +0200

    Fixed animations

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false
 };
 
-export const baseUrl = 'http://83.215.54.212:3000/';
+export const baseUrl = 'https://localhost:3000/';
 
 /*
  * For easier debugging in development mode, you can import the following file
